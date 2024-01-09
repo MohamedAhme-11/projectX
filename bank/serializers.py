66ad20c8523e2543f9911d71d829b05ed74466a6
Mohamed Ahmed <81529376+MohamedAhme-11@users.noreply.github.com>
2024-01-09 16:09:36 +0200

    Update serializers.py

--- bank/serializers.py
+++ bank/serializers.py
@@ -1,106 +1,66 @@
-# serializers.py
 from rest_framework import serializers
-from .model import CustomUser, Legislator, FacultyCriteria, MajorCriteria, CourseCriteria, Course
 from django.contrib.auth.hashers import make_password
+from cities.models import City
+from .model import CustomUser, Legislator, FacultyCriteria, MajorCriteria, CourseCriteria
+from rest_framework.exceptions import ValidationError
 
-class LegislatorSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    
-    # New fields
-    location_branch_name_en = serializers.CharField()
-    location_branch_name_ar = serializers.CharField()
-    primary_contact_name = serializers.CharField()
-    website = serializers.URLField()
-    country = serializers.CharField()
-    governorate = serializers.CharField()
-    city = serializers.CharField()
-    phone = serializers.CharField()
-    landline = serializers.CharField()
-    show_on_qodourat = serializers.BooleanField()
+class LegislatorRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField( write_only=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'})  # Confirm password field
+    city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), required=False)
+    country = serializers.CharField(default="Default Country")
 
     class Meta:
         model = Legislator
-        fields = ['name_en', 'name_ar', 'email', 'password', 'password2',
-                  'location_branch_name_en', 'location_branch_name_ar', 
-                  'primary_contact_name', 'website', 'country', 'governorate', 
-                  'city', 'phone', 'landline', 'show_on_qodourat']
+        fields = ('name_en', 'name_ar', 'email', 'city', 'password', 'password2', 
+                  'location_branch_name_en', 'location_branch_name_ar', 'primary_contact_name',
+                  'website', 'country', 'show_on_qodourat')  # Add 'password2' to fields
 
     def validate(self, attrs):
-        if attrs['password'] != attrs.pop('password2'):
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        attrs['password'] = make_password(attrs['password'])
+        if attrs['password'] != attrs.pop('password2', None):
+            raise ValidationError({"password2": "Password fields didn't match."})
         return attrs
 
     def create(self, validated_data):
-        # Creating user data and removing legislator-specific data
         user_data = {
-            'username': validated_data.get('email'),  # Assuming email is used as username
-            'email': validated_data.pop('email'),
-            # ... include other user-specific fields if necessary ...
+            'username': validated_data.get('email'),
+            'email': validated_data.get('email'),
+            'first_name': validated_data.get('name_en', ''),
+            'last_name': validated_data.get('name_ar', ''),
+            # ... potentially other fields ...
         }
-        password = validated_data.pop('password')
-        validated_data.pop('password2', None)
-
-        # Create CustomUser instance
         user = CustomUser.objects.create(**user_data)
-        user.set_password(password)
+        user.set_password(validated_data['password'])
         user.save()
 
-        # Create Legislator instance linked to the user
-        # Ensure only legislator-specific fields are left in validated_data
-        legislator = Legislator.objects.create(user=user, **validated_data)
+        # Extract city and use it to set the country
+        city = validated_data.get('city')
+        if city:
+            # Make sure your City model has a 'country' field that points to a Country model
+            validated_data['country'] = city.country.name
 
+        # Remove password and password2 fields as they are write-only and should not be passed to create()
+        validated_data.pop('password', None)
+        validated_data.pop('password2', None)
+
+        # Create the Legislator object with the modified validated_data
+        legislator_data = validated_data
+        legislator_data['user'] = user
+        legislator = Legislator.objects.create(**legislator_data)
         return legislator
 
+    # ... [rest of the code like validate method] ...
 class FacultyCriteriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = FacultyCriteria
-        fields = '__all__'  # Include the fields you want to expose
+        fields = '__all__'
 
 class MajorCriteriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = MajorCriteria
-        fields = '__all__'  # Include the fields you want to expose
+        fields = '__all__'
 
 class CourseCriteriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = CourseCriteria
-        fields = '__all__'  # Include the fields you want to expose
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
         fields = '__all__'
-    
-    def create(self, validated_data):
-        # Handle list of course data
-        courses = [Course(**item) for item in validated_data]
-        return Course.objects.bulk_create(courses)
-
-
-class FacultyCriteriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FacultyCriteria
-        fields = '__all__'  # Include the fields you want to expose
-
-class MajorCriteriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MajorCriteria
-        fields = '__all__'  # Include the fields you want to expose
-
-class CourseCriteriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CourseCriteria
-        fields = '__all__'  # Include the fields you want to expose
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-    
-    def create(self, validated_data):
-        # Handle list of course data
-        courses = [Course(**item) for item in validated_data]
-        return Course.objects.bulk_create(courses)
-    
