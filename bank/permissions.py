b55df7ddddc49d4adf855f506bb82e53f3707f0f
Mohamed Ahmed <81529376+MohamedAhme-11@users.noreply.github.com>
2024-01-09 16:09:51 +0200

    Update permissions.py

--- bank/permissions.py
+++ bank/permissions.py
@@ -1,7 +1,5 @@
-# permissions.py
 from rest_framework import permissions
 
 class IsLegislator(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Implement logic to determine if the user is a legislator
-        return request.user.groups.filter(name='Legislators').exists() or request.user.is_legislator
+        return request.user.is_authenticated and hasattr(request.user, 'legislator')
