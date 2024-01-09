31e47a535e878394bcba931e4dff3f497ded4281
Mohamed Ahmed <81529376+MohamedAhme-11@users.noreply.github.com>
2024-01-09 16:08:21 +0200

    Update views.py

--- bank/views.py
+++ bank/views.py
@@ -1,87 +1,71 @@
-# views.py
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from .model import Legislator, FacultyCriteria, MajorCriteria, CourseCriteria
-from .serializers import (
-    LegislatorSerializer,
-    FacultyCriteriaSerializer,
-    MajorCriteriaSerializer,
-    CourseCriteriaSerializer,
-)
+from .model import Legislator,FacultyCriteria, MajorCriteria, CourseCriteria
+from .serializers import LegislatorRegistrationSerializer,FacultyCriteriaSerializer, MajorCriteriaSerializer, CourseCriteriaSerializer
+# views.py
 from .permissions import IsLegislator
 
-class LegislatorViewSet(viewsets.ViewSet):
-    """
-    A viewset that provides actions for Legislator to register, login,
-    and manage criteria for faculty, major, and course.
-    """
 
-    permission_classes_by_action = {
-        'create': [permissions.AllowAny],
-        'add_faculty_criteria': [IsLegislator],
-        'add_major_criteria': [IsLegislator],
-        'add_course_criteria': [IsLegislator],
-        # Add other actions if necessary
-    }
+class LegislatorViewSet(viewsets.ModelViewSet):
+    queryset = Legislator.objects.all()
+    serializer_class = LegislatorRegistrationSerializer
+    authentication_classes = [TokenAuthentication]
+    lookup_field = 'slug' 
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        legislator = serializer.save()
+        token, created = Token.objects.get_or_create(user=legislator.user)
+        return Response({
+            "legislator": LegislatorRegistrationSerializer(legislator, context=self.get_serializer_context()).data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
 
-    def create(self, request):
-        """
-        Register a new legislator and return a token for authentication.
-        """
-        serializer = LegislatorSerializer(data=request.data)
-        if serializer.is_valid():
-            legislator = serializer.save()
-            token, created = Token.objects.get_or_create(user=legislator.user)
-            return Response({
-                "message": "Legislator successfully registered",
-                "token": token.key
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
+    def login(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(username=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    @action(detail=True, methods=['PUT'], permission_classes=[permissions.IsAuthenticated])
+    def update_account(self, request, pk=None):
+        legislator = self.get_object()
+        serializer = self.get_serializer(legislator, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-    @action(detail=False, methods=['post'], url_path='add-faculty-criteria')
-    def add_faculty_criteria(self, request):
-        """
-        Add new faculty criteria linked to the legislator.
-        """
-        serializer = FacultyCriteriaSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(legislator=request.user.legislator)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def destroy(self, request, *args, **kwargs):
+        legislator = self.get_object()
+        user = legislator.user
+        self.perform_destroy(legislator)
+        user.delete()  # Delete the associated user as well
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class FacultyCriteriaViewSet(viewsets.ModelViewSet):
+    queryset = FacultyCriteria.objects.all()
+    serializer_class = FacultyCriteriaSerializer
+    permission_classes = [IsLegislator]
+    # Add permission classes as needed
 
-    @action(detail=False, methods=['post'], url_path='add-major-criteria')
-    def add_major_criteria(self, request):
-        """
-        Add new major criteria linked to the legislator.
-        """
-        serializer = MajorCriteriaSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(legislator=request.user.legislator)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MajorCriteriaViewSet(viewsets.ModelViewSet):
+    queryset = MajorCriteria.objects.all()
+    serializer_class = MajorCriteriaSerializer
+    permission_classes = [IsLegislator]
+    # Add permission classes as needed
 
-    @action(detail=False, methods=['post'], url_path='add-course-criteria')
-    def add_course_criteria(self, request):
-        """
-        Add new course criteria linked to the legislator.
-        """
-        serializer = CourseCriteriaSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(legislator=request.user.legislator)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get_permissions(self):
-        """
-        Instantiates and returns the list of permissions that this view requires.
-        """
-        try:
-            # return permission_classes depending on `action`
-            return [permission() for permission in self.permission_classes_by_action[self.action]]
-        except KeyError:
-            # action is not set return default permission_classes
-            return [permission() for permission in self.permission_classes]
-
+class CourseCriteriaViewSet(viewsets.ModelViewSet):
+    queryset = CourseCriteria.objects.all()
+    serializer_class = CourseCriteriaSerializer
+    permission_classes = [IsLegislator]
+    # Add permission classes as needed
